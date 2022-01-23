594e04776dcc2da5541a322a750143f6782f2ef1
Trent Savage <trent.savage@aggiemail.usu.edu>
2022-01-22 18:45:24 -0700

    got ride of shivering cursor

--- GameLoop/MyGame.cs
+++ GameLoop/MyGame.cs
@@ -3,10 +3,15 @@
 public class MyGame
 {
     string input { get; set; }
-
+    bool enterFlg { get; set; }
+    bool newRender { get; set; }
+    int CursorStartingPositionConstant = 6;
 	public MyGame()
 	{
-	}
+        input = "";
+        enterFlg = false;
+        newRender = false;
+    }
 
     public void initialize()
     {
@@ -26,18 +31,13 @@ public class MyGame
 
     public void processInput()
     {
-        if (Console.KeyAvailable == false)
+        if (Console.KeyAvailable == true)
         {
+            newRender = true;
             ConsoleKeyInfo key = Console.ReadKey();
             if (key.Key == ConsoleKey.Backspace)
             {
-                input.Remove(input.Length - 1,1);
-            }
-            else if(key.Key == ConsoleKey.Enter)
-            {
-                // Most efficent to put this logic here.
-                Console.SetCursorPosition(0, Console.CursorTop+1);
-                input = "";
+                this.input.Remove(input.Length - 1,1);
             }
             else
             {
@@ -50,33 +50,37 @@ public class MyGame
     public void update(/*TimeSpan elapsedTime*/)
     {
         //check if last character on input is the Enter char
-        if (input.EndsWith("\r\n"))
+        if (this.input.EndsWith("\r"))
         {
-            input = "";
-            // and then set cursorx to be after the "[cmd]"
+            this.input = "";
+            this.enterFlg = true;
+
         }
-    }
-    public void render() 
-    {
-        // remember where the cursor was to know where it will be
-        //cursorx = Console.CursorLeft;
-        //cursory = Console.CursorTop;
-
-        // wipe the old image off (NOTE: There is a better way to do this with string formatting, but I am lazy)
-        // See https://docs.microsoft.com/en-us/dotnet/api/system.string.format?view=net-6.0 for more on that later.
-        Console.SetCursorPosition(0, Console.CursorTop);
-        Console.WriteLine(new string(' ', Console.WindowWidth));
-
-        // draw the new image
-        Console.SetCursorPosition(0, Console.CursorTop - 1);
-        Console.Write("[cmd:]"+input);
-
-        // 
-        //Console.SetCursorPosition(cursorx, Console.CursorTop - 1);
     }
 
     /// <summary>
     /// Thanks dknaak
     /// https://stackoverflow.com/questions/8946808/can-console-clear-be-used-to-only-clear-a-line-instead-of-whole-console/8946847
     /// </summary>
+    public void render() 
+    {
+        if (newRender)
+        {
+
+            // draw the new line over top of old line
+            if (enterFlg)
+                Console.CursorTop++;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine(("[cmd:]" + input).PadRight(Console.WindowWidth));
+
+            Console.SetCursorPosition(CursorStartingPositionConstant + input.Length, Console.CursorTop - 1);
+
+
+
+            // Reset flags
+            this.enterFlg = false;
+        }
+        newRender = false;
+    }
+
 }
